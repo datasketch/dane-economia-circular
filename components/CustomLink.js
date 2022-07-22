3432818ba36b01bed9ae1d59c038716e20e118d7
David Daza <03dazal@gmail.com>
2022-07-22 11:40:17 -0500

    Update CustomLink component

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -1,15 +1,21 @@
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import React, { Children, useEffect, useState } from 'react'
 
 export default function CustomLink ({ children, ...restProps }) {
+  const child = Children.only(children)
   const [target, setTarget] = useState('_self')
+
   useEffect(() => {
     const isIframe = window.self !== window.top
     if (isIframe) setTarget('_top')
   }, [])
+
   return (
-    <Link {...restProps} target={target}>
-      {children}
+    <Link {...restProps}>
+      {React.cloneElement(child, {
+        ...child.props,
+        target
+      })}
     </Link>
   )
 }
